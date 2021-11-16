1ec87f7926226cf06688314d07a16ec45af38e62
Travis Puryear <travispuryear678@gmail.com>
2021-11-15 20:05:07 -0500

    Color changes.

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const pokeNumbers = 151;
 const container = document.querySelector(".pokemon-container");
 const colors = {
-    fire:'#E85211',
-    grass:'#25E811',
-    electric: "#FFF230",
-    water:"blue",
-    bug: "green",
-    poison:"purple",
-    ice: "#5EFAF7",
-    rock: "brown",
-    normal: "gray",
-    ground:"brown",
-    fairy:"pink",
-    fighting:"orange",
-    psychic:"#261385",
-    dragon:"#DC1B5C"
+  fire: "#E85211",
+  grass: "#25E811",
+  electric: "#FFF230",
+  water: "blue",
+  bug: "green",
+  poison: "purple",
+  ice: "#5EFAF7",
+  rock: "brown",
+  normal: "gray",
+  ground: "brown",
+  fairy: "pink",
+  fighting: "orange",
+  psychic: "#261385",
+  dragon: "#DC1B5C",
+};
 
-}
-
-const main_types = Object.keys(colors)
+const main_types = Object.keys(colors);
 
 console.log(main_types);
-
-
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokeNumbers; i++) {
@@ -42,20 +39,20 @@ fetchPokemon();
 function create(pokemon) {
   const pokemonEL = document.createElement("div");
   pokemonEL.classList.add("pokemonBox");
-  const poke_types = pokemon.types.map(el => el.type.name)
-const type = main_types.find(type => poke_types.indexOf(type)> -1)
+  const poke_types = pokemon.types.map((el) => el.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
   const pokeInnerHtml = `
    
     
     <h1 class="pokemonName">${capitalizeFirstLetter(pokemon.name)}</h1> 
     <p>Height: ${heightConversionFeet(pokemon.height)}'Ft</p> 
     <img class="pokeimg" src=${pokemon.sprites.front_default}>
-     <p>${(poke_types)}</p>
+     <p class="type">${poke_types}</p>
     
     `;
   pokemonEL.innerHTML = pokeInnerHtml;
-  const color = colors[type]
-    pokemonEL.style.backgroundColor = color;
+  const color = colors[type];
+  pokemonEL.style.backgroundColor = color;
   container.appendChild(pokemonEL);
 }
 
@@ -67,5 +64,5 @@ function heightConversionFeet(decimeter) {
 }
 
 function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
