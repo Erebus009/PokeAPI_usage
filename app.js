5b0b0e6c8d5caf1d5d11ab968a67989eca9c5f78
Travis Puryear <travispuryear678@gmail.com>
2021-11-15 19:47:23 -0500

    Color on type.

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const pokeNumbers = 151;
 const container = document.querySelector(".pokemon-container");
+const colors = {
+    fire:'#E85211',
+    grass:'#25E811',
+    electric: "#FFF230",
+    water:"blue",
+    bug: "green",
+    poison:"purple",
+    ice: "#5EFAF7",
+    rock: "brown",
+    normal: "gray",
+    ground:"brown",
+    fairy:"pink",
+    fighting:"orange",
+    psychic:"#261385",
+    dragon:"#DC1B5C"
+
+}
+
+const main_types = Object.keys(colors)
+
+console.log(main_types);
+
+
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokeNumbers; i++) {
@@ -11,7 +34,6 @@ const GETPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const pokemon = await res.json();
-  console.log(pokemon);
   create(pokemon);
 };
 
@@ -20,17 +42,20 @@ fetchPokemon();
 function create(pokemon) {
   const pokemonEL = document.createElement("div");
   pokemonEL.classList.add("pokemonBox");
-
+  const poke_types = pokemon.types.map(el => el.type.name)
+const type = main_types.find(type => poke_types.indexOf(type)> -1)
   const pokeInnerHtml = `
    
     
-    ${capitalizeFirstLetter(pokemon.name)} || Height: ${heightConversionFeet(pokemon.height)}'Ft
-    <img src=${pokemon.sprites.front_default}>
-     ${pokemon.types[0].type.name}
+    <h1 class="pokemonName">${capitalizeFirstLetter(pokemon.name)}</h1> 
+    <p>Height: ${heightConversionFeet(pokemon.height)}'Ft</p> 
+    <img class="pokeimg" src=${pokemon.sprites.front_default}>
+     <p>${(poke_types)}</p>
     
     `;
   pokemonEL.innerHTML = pokeInnerHtml;
-
+  const color = colors[type]
+    pokemonEL.style.backgroundColor = color;
   container.appendChild(pokemonEL);
 }
 
