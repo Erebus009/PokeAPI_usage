cbfb6660467ed38ffe30d9d7540d5fbffeb999b1
Travis Puryear <travispuryear678@gmail.com>
2022-01-08 08:02:20 -0500

    Updates

---
+++ precache-manifest.a699be242c6eda7b3894e588a3215d86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5752ac52e4b2066b4d1915616143e6a",
+    "url": "/PokeDex-with-React/index.html"
+  },
+  {
+    "revision": "6871bc1641c6bb9474b6",
+    "url": "/PokeDex-with-React/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "b66cad8e49c0097f90ee",
+    "url": "/PokeDex-with-React/static/css/main.c1e2227f.chunk.css"
+  },
+  {
+    "revision": "6871bc1641c6bb9474b6",
+    "url": "/PokeDex-with-React/static/js/2.82bb7dc3.chunk.js"
+  },
+  {
+    "revision": "b66cad8e49c0097f90ee",
+    "url": "/PokeDex-with-React/static/js/main.937d31cb.chunk.js"
+  },
+  {
+    "revision": "724d3bf8d529a0e259bf",
+    "url": "/PokeDex-with-React/static/js/runtime~main.6da03eab.js"
+  },
+  {
+    "revision": "fa5076f0751d9f9eb30dd06ab4b516d3",
+    "url": "/PokeDex-with-React/static/media/4.fa5076f0.svg"
+  },
+  {
+    "revision": "6bfce2e2ebafeae1aac0c16788499a74",
+    "url": "/PokeDex-with-React/static/media/spinner.6bfce2e2.gif"
+  }
+]);
